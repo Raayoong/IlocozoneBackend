7f864dd043c6f5cca2fc9890d8f49e88d98e6ec1
Raayoong <rclyde.campos@gmail.com>
2024-12-06 00:06:29 +0800

    add tel no field

--- routes/request_order_routes.js
+++ routes/request_order_routes.js
@@ -4,10 +4,10 @@ var router = express.Router();
 
 // INSERT NEW REQUEST ORDER
 router.post('/new/request_order', (req,res,next)=> {
-    const sql = 'INSERT INTO `request_orders`(`work_type`, `ref_no`, `recovery_model`, `recovery_serial`, `used_serial`, `category`, `status`) VALUES (?,?,?,?,?,?, 0)'
+    const sql = 'INSERT INTO `request_orders`(`work_type`, `ref_no`, `recovery_model`, `recovery_serial`, `used_serial`, `category`,`tel_no`, `status`) VALUES (?,?,?,?,?,?,?, 0)'
     const db = require('../app');
-    const {work_type, ref_no, recovery_model, recovery_serial, used_serial, category} = req.body
-    db.query(sql, [work_type, ref_no, recovery_model, recovery_serial, used_serial, category], (err, data)=> {
+    const {work_type, ref_no, recovery_model, recovery_serial, used_serial, category,tel_no} = req.body
+    db.query(sql, [work_type, ref_no, recovery_model, recovery_serial, used_serial, category, tel_no], (err, data)=> {
         if(err){
             res.status(400).send(err.message)
             console.log(err.message)
