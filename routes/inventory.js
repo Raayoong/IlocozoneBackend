411ca110624348f242db97ea1260d430074844b4
Raayoong <rclyde.campos@gmail.com>
2025-01-03 18:11:18 +0800

    add filter sql

--- routes/inventory.js
+++ routes/inventory.js
@@ -68,7 +68,7 @@ router.put('/update/inventory/:usedSerial', (req,res,next)=> {
 })
 
 // get brand and model
-router.get(`/inventory/filter`, (req, res)=> {
+router.get(`/inventory/filter/:category`, (req, res)=> {
   const category = req.params.category
   const sql = `SELECT DISTINCT brand, model from inventory WHERE category=${category}`
   const db = require('../app');
